8d7ee163f4cdb5ed0ad4776d24fc33c7969517b8
Paulo Pitte <pauloanalista@outlook.com>
2021-04-11 13:58:19 -0300

    Adicionar arquivos de projeto.

---
+++ WorkerAPIBackgroudServices/Controllers/HealthCheckController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace WorkerAPIBackgroudServices.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class HealthCheckController : ControllerBase
+    {
+     
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok($"HealthCheck - {DateTime.UtcNow:o}" );
+        }
+    }
+}
